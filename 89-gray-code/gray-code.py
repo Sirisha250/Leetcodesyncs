0a78a427326508d21f3cce294840965776bd75e4
Sirisha250 <bunnysirireddy@gmail.com>
2025-02-07 12:10:51 +0530

    Time: 8 ms (58.77%) | Memory: 20.8 MB (30.84%) - LeetSync

---
+++ 89-gray-code/gray-code.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def grayCode(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        return [i ^ (i >> 1) for i in range(2**n)]
+
+        
