ca2c226a64c2a808b5131f59e28748a7a4e30288
Sirisha250 <bunnysirireddy@gmail.com>
2025-02-12 12:07:18 +0530

    Time: 36 ms (47.39%) | Memory: 12.7 MB (84.62%) - LeetSync

---
+++ 133-clone-graph/clone-graph.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, val=0, neighbors=None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+class Solution:
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return None
+        
+        visited = {}
+
+        def dfs(curr):
+            if curr in visited:
+                return visited[curr]
+
+            copy = Node(curr.val)
+            visited[curr] = copy
+            
+            for neighbor in curr.neighbors:
+                copy.neighbors.append(dfs(neighbor))
+            
+            return copy
+
+        return dfs(node)
+
+        
