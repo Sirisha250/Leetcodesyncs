816bdc50b5d8e6af3db6e7c0b9c2d6111bac1d5d
Sirisha250 <bunnysirireddy@gmail.com>
2025-02-27 10:57:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (99.26%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -0,0 +1,39 @@
+from collections import deque
+
+class MyStack(object):
+
+    def __init__(self):
+        """Initialize the stack using a queue."""
+        self.queue = deque()
+
+    def push(self, x):
+        """
+        Push element x onto stack.
+        :type x: int
+        :rtype: None
+        """
+        self.queue.append(x)  # Push element to the queue
+        # Rotate the queue to maintain stack order
+        for _ in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self):
+        """
+        Removes the element on top of the stack and returns it.
+        :rtype: int
+        """
+        return self.queue.popleft()
+
+    def top(self):
+        """
+        Get the top element.
+        :rtype: int
+        """
+        return self.queue[0]
+
+    def empty(self):
+        """
+        Returns whether the stack is empty.
+        :rtype: bool
+        """
+        return len(self.queue) == 0
